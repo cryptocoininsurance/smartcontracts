371ef62f97965f7f8dfbcde26975bf4096c5495d
Maxim S <maxsizmobile@gmail.com>
2018-09-26 11:47:51 +0800

    remove transferOwnerShip from Ownable

--- contracts/Cryptocoin.sol
+++ contracts/Cryptocoin.sol
@@ -282,16 +282,6 @@ contract Ownable {
     _;
   }
 
-  /**
-   * @dev Allows the current owner to transfer control of the contract to a newOwner.
-   * @param newOwner The address to transfer ownership to.
-   */
-  function transferOwnership(address newOwner) public onlyOwner {
-    require(newOwner != address(0));
-    emit OwnershipTransferred(owner, newOwner);
-    owner = newOwner;
-  }
-
 }
 
 contract Claimable is Ownable {
